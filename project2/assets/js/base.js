dce89ccbbd29ed9d7d97ca6216eb87b7c05ab8b5
elissaeren <elissasorenson@dyn-192-168-236-95.wifi.wou.edu>
2018-02-25 17:31:15 -0800

    Added more files

---
+++ project2/assets/js/base.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    // list one content
+			var one = [
+                "",
+			];
+
+			// This is a very common randomizing function.
+			// It takes a list (array) and returns one at random.
+			function select_random(x){
+				y = x[Math.floor(Math.random()*x.length)];
+				return y;
+			}
+
+			function generate(){
+
+				// Select a random item from each list
+				var selected_one = select_random(one);
+
+				// Take the selected (random) item from list and make it visible
+				$('.list-one').html(selected_one);
+
+			}
+
+			$('#quit').click(function(){
+				generate();
+			});
+
+			generate();
+			
+		});
